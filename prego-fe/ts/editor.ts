de75268eea9865af589d6e795e4ea07441fbbc04
Syafiq <ahmadsyafiq93@gmail.com>
2021-10-22 19:24:41 +0800

    Configure wysiwyg editor

--- prego-fe/ts/editor.ts
+++ prego-fe/ts/editor.ts
@@ -1,3 +1,21 @@
-import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import Quill, { QuillOptionsStatic } from "quill";
 
-const editor = new Quill("#editor")
+const options: QuillOptionsStatic = {
+    theme: "snow",
+    placeholder: "Insert text here..",
+    modules: {
+        toolbar: [
+            [{ "header": [1, 2, 3, 4, 5, 6, false] }],
+
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code-block"],
+
+            [{ "list": "ordered" }, { "list": "bullet" }],
+
+            ["image"],
+        ]
+    },
+};
+
+new Quill("#editor", options)
