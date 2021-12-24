c68bb52229f949f18f92d169800f4bf0476f15a8
Syafiq <ahmadsyafiq93@gmail.com>
2021-12-25 00:16:48 +0800

    Sassy

--- prego-fe/ts/editor.ts
+++ prego-fe/ts/editor.ts
@@ -11,7 +11,7 @@ const options: QuillOptionsStatic = {
             [{ "header": [1, 2, 3, 4, 5, 6, false] }],
 
             ["bold", "italic", "underline", "strike", "link"],
-            ["blockquote", "code-block"],
+            ["blockquote", "code", "code-block"],
 
             [{ "list": "ordered" }, { "list": "bullet" }],
 
