d99cf7e52d8a8da91c63cc566a762d9f6891647a
Syafiq <ahmadsyafiq93@gmail.com>
2022-01-03 17:36:16 +0800

    Make code highliting "better"

--- prego-fe/ts/codehighlight.ts
+++ prego-fe/ts/codehighlight.ts
@@ -1,7 +1,7 @@
 import hljs from "highlight.js";
 import "highlight.js/styles/github-dark.css"
 
-hljs.configure({ languages: ["python",] })
+hljs.configure({ languages: ["python", "javascript"] })
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("pre.ql-syntax").forEach((el: HTMLElement) => {
